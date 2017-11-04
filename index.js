63c9641f81839d9a81bcfdbb502ad768e74c3d36
Aayush <aayushjaiswal0114@gmail.com>
2017-11-04 20:57:00 +0530

    Completed

--- index.js
+++ index.js
@@ -10,11 +10,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
 app.get('/',(req,res)=>{
-    res.render('city');
+    res.render('city', {weather: null, error: null});
 });
 
 app.post('/weather/details',(req,res)=>{
     let city = req.body.city;
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${key}`; 
+    request(url,(error,response,body)=>{
+        if(error){
+            res.render('city',{weather:null,error:'Try again'})
+        }else{
+            let weather = JSON.parse(body);
+            if(weather.main == undefined){
+                res.render('city',{weather:null,error:'Try again'});
+            }else{
+                let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+                res.render('city', {weather: weatherText, error: null});
+            }
+            }
+        });
+
+    
 });
 app.listen('3000');

fca5baedfc96e2e63b656e66782e23d6d24c39f5
Aayush <aayushjaiswal0114@gmail.com>
2017-11-04 20:35:10 +0530

    Routes updated

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ const key = keys.weather.key;
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
+
+app.get('/',(req,res)=>{
+    res.render('city');
+});
+
+app.post('/weather/details',(req,res)=>{
+    let city = req.body.city;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${key}`; 
+});
+app.listen('3000');
